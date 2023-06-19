50d7ef467216a0fd71987955dad668660a79fb98
carlosmoyav <carlosmoyaec@gmail.com>
2023-06-19 12:24:59 -0500

    subir

---
+++ 2do Parcial/Talleres_en_clase/8graphQL/graph-student/src/estudiantes/dto/inputs/create-estudiante.input.ts
@@ -0,0 +1,18 @@
+import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, IsPositive } from 'class-validator';
+
+@InputType()
+export class CreateEstudianteInput {
+
+  @Field(()=>String )
+  @IsNotEmpty()
+  identificacion:string;
+
+  @Field(()=>String )
+  @IsNotEmpty()
+  nombre:string;
+
+  @Field(()=>Boolean )
+  @IsOptional()
+  estado:boolean;
+}
